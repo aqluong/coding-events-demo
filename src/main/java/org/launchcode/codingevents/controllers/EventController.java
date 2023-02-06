570c6ba4809af44eb68a34043cd1e2445b2fc7c3
Anthony Luong <anthony@Anthonys-MacBook-Pro.local>
2023-02-05 21:55:11 -0500

    Complete 13.5

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,3 +1,5 @@
+
+
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
@@ -21,21 +23,24 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
+    // lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String renderCreateEventForm() {
         return "events/create";
+
+    }
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:/events";
     }
 
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
-    }
+
+
 
 }
